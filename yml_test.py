98a037b5ee1d2eb5ca5beab0e9006d84d27fda4c
TerminalTesting <testing@terminal.ru>
2014-05-21 16:07:26 +0600

    fix

--- yml_test.py
+++ yml_test.py
@@ -12,6 +12,7 @@ from yml_models import *
 from string import split
 from urlparse import urlparse
 from sqlalchemy import or_
+from sqlalchemy import func
 
 def get_domain(url):
     """  найти домен региона """
@@ -400,12 +401,25 @@ class YMLTest(unittest.TestCase):
                                 print 'Цена доставки не соответствует'
                                 print 'ID товара: ', element.attrib['id'] ,' значение в файле:', delivery_price_tag.text, ' значение в базе данных:', cost
                                 print '-'*80
+                                
                         else:
-                            stat+=1
-                            print 'Ошибка в теге <LOCAL_DELIVERY_COST>:'
-                            print 'Цена доставки не найдена в БД'
-                            print 'ID товара: ', element.attrib['id']
-                            print '-'*80
+                            cost = session.query(func.max(Rates.cost)).\
+                                      filter(Rates.city_id == DPDcity).first()
+                            if cost:
+                                cost = cost[0]#tuple is result of query
+                                if int(delivery_price_tag.text) != cost: 
+                                    stat+=1
+                                    print 'Ошибка в теге <LOCAL_DELIVERY_COST>:'
+                                    print 'Цена доставки не соответствует'
+                                    print 'ID товара: ', element.attrib['id'] ,' значение в файле:', delivery_price_tag.text, ' значение в базе данных:', cost
+                                    print '-'*80
+                                    
+                            else:
+                                stat+=1
+                                print 'Ошибка в теге <LOCAL_DELIVERY_COST>:'
+                                print 'Цена доставки не найдена в БД'
+                                print 'ID товара: ', element.attrib['id'], cost
+                                print '-'*80
                             
                     else:
                         if int( item[8] ) > 0:
